34e1aee2dbc9f4fcbba49b7c59c336a1da82e2b1
silentwolf-dev <omariimanibrown421@gmail.com>
2024-12-02 17:13:11 -0500

    time to have fun

--- api/routes/recipe.ts
+++ api/routes/recipe.ts
@@ -1,10 +1,7 @@
 /** @format */
 
 import { Router } from "express";
-import recipeRoutes from "./recipe";
 
 const router = Router();
-
-router.use("/recipe", recipeRoutes);
 
 export default router;
