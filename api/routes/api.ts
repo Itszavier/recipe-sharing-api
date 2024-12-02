bb47cd9e9ebd670e091c0e045d9edced5a057a79
silentwolf-dev <omariimanibrown421@gmail.com>
2024-12-02 17:24:26 -0500

    small changes

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -2,8 +2,11 @@
 
 import { Router } from "express";
 import recipeRoutes from "./recipe";
+import apiKeyAuth from "../middleware/apikeyToken";
 
 const router = Router();
+
+router.use(apiKeyAuth);
 
 router.use("/recipe", recipeRoutes);
 
--- api/routes/recipe.ts
+++ api/routes/recipe.ts
@@ -1,7 +1,27 @@
 /** @format */
 
 import { Router } from "express";
+import { prisma } from "../db";
 
 const router = Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const recipes = await prisma.recipe.findMany();
+
+    res
+      .status(200)
+      .json({ message: "--> here is the list ", recipes });
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/create", (req, res, next) => {
+    
+});
+
+router.delete("/delete", (req, res, next) => {});
 
 export default router;
