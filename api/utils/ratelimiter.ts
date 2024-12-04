dfd0d76fa080217687ca8c51fc821e4c61d0bf8f
silentwolf-dev <omariimanibrown421@gmail.com>
2024-12-03 21:37:26 -0500

    change ratelimit message

--- api/utils/ratelimiter.ts
+++ api/utils/ratelimiter.ts
@@ -7,5 +7,5 @@ export const requestLimiter = rateLimit({
   limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  message: "Too many login attempts, please try again later.",
+  message: "Too many attempts, please try again later.",
 });
